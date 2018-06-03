c3aa15791a7d2ae15e0b3c423b81b26c7cbd28e7
Ian Fraser <icf0001@auburn.edu>
2018-06-03 18:03:50 -0500

    Finalized Todo list

--- assets/js/todosBackground.js
+++ assets/js/todosBackground.js
@@ -1,1 +1,28 @@
-console.log("hi");
+$("#todoList").on("click", "li", function()
+{
+	$(this).toggleClass("completedItem")
+});
+
+$("#todoList").on("click", "span", function(event)
+{
+	$(this).parent().fadeOut(700, function()
+	{
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event)
+{
+	if(event.which === 13)
+	{
+		var newTodo = $(this).val();
+		$(this).val("");
+		$("#todoList").append("<li><span><i class='fa fa-trash'></i></span> " + newTodo + "</li>")
+	}
+});
+
+$(".fa-plus").click(function()
+{
+	$("input[type='text']").fadeToggle();
+});
